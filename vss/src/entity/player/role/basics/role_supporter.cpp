4e3990484b4a9dee52d01714a9a75368ddcf740e
Amanda Moraes <mandynhamoraes@hotmail.com>
2020-09-21 16:06:26 -0300

    redefined role Supporter using ball velocity, ball distance to our goal and player distance to our goal as parameters to enable a behavior (assistant or barrier)

--- vss/src/entity/player/role/basics/role_supporter.cpp
+++ vss/src/entity/player/role/basics/role_supporter.cpp
@@ -7,58 +7,37 @@ Role_Supporter::Role_Supporter()
 {
     _bh_br = nullptr;
     _bh_as = nullptr;
-    _bh_sb = nullptr;
 
 }
 void Role_Supporter::initializeBehaviours(){
     usesBehaviour(BHV_ASSISTANT, _bh_as = new Behaviour_Assistant());
     usesBehaviour(BHV_BARRIER , _bh_br = new Behaviour_Barrier());
-    usesBehaviour(BHV_STAYBACK , _bh_sb = new Behaviour_StayBack());
 }
 void Role_Supporter::configure(){
-    setBehaviour(BHV_ASSISTANT); //initial behaviour
-
-    //Coordenadas da quina de Cima
-    TopCorner_X = loc()->ourFieldTopCorner().x();
-    TopCorner_Y = loc()->ourFieldTopCorner().y();
-    //Coordenadas da quina de Baixo
-     BottomCorner_X = loc()->ourFieldBottomCorner().x();
-    BottomCorner_Y = loc()->ourFieldBottomCorner().y();
-    //Coordenadas auxiliares da Trave esquerda
-     Post_X = loc()->ourGoalLeftPost().x();
-     Post_Y = loc()->ourGoalLeftPost().y();
-    //distancia constante da quina pra trave
-
-     dist_Corner_LeftPost = sqrt(pow((TopCorner_X - Post_X), 2) + pow((TopCorner_Y - Post_Y) , 2 )) ;
-
+    setBehaviour(BHV_BARRIER); //initial behaviour
 }
 void Role_Supporter::run(){
-
-    //Areas das quinas
-    float distBallfromTopCorner = sqrt(pow((TopCorner_X - loc()->ball().x()), 2) + pow((TopCorner_Y - loc()->ball().y()) , 2 ));
-    float distBallfromBottomCorner = sqrt(pow((BottomCorner_X - loc()->ball().x()), 2) + pow((BottomCorner_Y - loc()->ball().y()) , 2 ));
-
-    if(distBallfromTopCorner <= dist_Corner_LeftPost || distBallfromBottomCorner <= dist_Corner_LeftPost){
-        if(EnemyHasBallPossession()){
+    //distancia da bola para o nosso gol, distancia do nosso jogador para o gol, se tem um inimigo mais proximo dela
+    if(EnemyHasBallPossession()){
+    //if(true){
+        float distBallFactor = 1.0f - loc()->distBallOurGoal()/WR::Utils::distance(loc()->fieldLeftTopCorner(), Position(true, loc()->fieldMaxX(), 0.0f, 0.0f));
+        float distPlayerFactor = 1.0f - player()->distOurGoal()/WR::Utils::distance(loc()->fieldLeftTopCorner(), Position(true, loc()->fieldMaxX(), 0.0f, 0.0f));
+        float ballVelFactor = loc()->ballVelocity().abs()/1.2f;
+        //std::cout << distBallFactor <<" "<< distPlayerFactor<<" "<< ballVelFactor << std::endl;
+        float avgFactor = (distBallFactor + distPlayerFactor + ballVelFactor)/3.0f;
+        if(avgFactor > 0.5f){
             setBehaviour(BHV_BARRIER);
-            //std::cout << "BHV_BARRIER" << std::endl;
-        }
-        else{
+        }else{
             setBehaviour(BHV_ASSISTANT);
-            //std::cout << "BHV_ASSISTANT" << std::endl;
         }
-
-    }
-
-    else{
+    }else{
         setBehaviour(BHV_ASSISTANT);
-        //std::cout << "BHV_ASSISTANT" << std::endl;
     }
 }
 bool Role_Supporter::EnemyHasBallPossession(){
     for(quint8 x = 0; x < VSSConstants::qtPlayers(); x++){
         if(PlayerBus::theirPlayerAvailable(x)){
-            if(PlayerBus::theirPlayer(x)->hasBallPossession()){
+            if(PlayerBus::theirPlayer(x)->isNearbyPosition(loc()->ball(), 0.09f)){
                 return true;
             }
         }
