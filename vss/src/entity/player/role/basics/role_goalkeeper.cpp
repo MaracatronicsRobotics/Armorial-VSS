062d996884c017806d2efa076cf2d2fa0b4490e8
Amanda Moraes <mandynhamoraes@hotmail.com>
2020-11-06 23:39:01 -0300

    role goalkeeper adapted to emit information based on signals received from the referee

--- vss/src/entity/player/role/basics/role_goalkeeper.cpp
+++ vss/src/entity/player/role/basics/role_goalkeeper.cpp
@@ -27,21 +27,198 @@ QString Role_Goalkeeper::name(){
 
 Role_Goalkeeper::Role_Goalkeeper() {
     _bh_gk = nullptr;
+    _bh_dn = nullptr;
+    _bh_tf = nullptr;
 }
 
 void Role_Goalkeeper::initializeBehaviours(){
     usesBehaviour(BHV_GK, _bh_gk = new Behaviour_Goalkeeper());
+    usesBehaviour(BHV_DONOTHING, _bh_dn = new Behaviour_DoNothing());
+    usesBehaviour(BHV_TAKEFOUL, _bh_tf = new Behaviour_TakeFoul());
 }
 
 void Role_Goalkeeper::configure(){
     isGK(true);
+    isNormalGame = true;
+    canGoBackToNormalGame = true;
+    lastFoul = VSSRef::KICKOFF;
+    weTake = true;
+    counter = 0;
 }
 
 void Role_Goalkeeper::run(){
-    setBehaviour(BHV_GK);
+    //if counter/timer is over or the goalkeeper has got out of our defense area
+    if(canGoBackToNormalGame || abs(player()->position().x()) <= (loc()->fieldMaxX() - loc()->fieldDefenseWidth() - 0.2f)){
+        counter = 0;
+        setBehaviour(BHV_GK);
+    }else{
+        if(counter >= 300){
+            counter = 0;
+            canGoBackToNormalGame = true;
+        }else{
+            counter++;
+        }
+    }
+}
+
+void Role_Goalkeeper::penaltyKick(Position* pos, Angle* ang){
+    //update isNormalGame variable (to false if foul is different from KICKOFF, STOP, GAME_ON, PENALTY)
+    isNormalGame = true;
+    //update lastFoul variable (last foul different from GAME_ON, STOP or KICK_OFF)
+    lastFoul = VSSRef::PENALTY_KICK;
+    //set behaviour doNothing so our player doesn't move from position emmited
+    setBehaviour(BHV_DONOTHING);
+
+    //put our goalkeeper in the middle of the goal
+    //if our side is right
+    if(loc()->ourSide().isRight()){
+        *pos = Position(true, loc()->fieldMaxX(), 0, 0);
+        *ang = Angle(true, float(M_PI_2));
+    }
+    //if our side is left
+    else{
+        *pos = Position(true, loc()->fieldMinX(), 0, 0);
+        *ang = Angle(true, float(M_PI_2));
+    }
+}
+
+void Role_Goalkeeper::goalKick(Position* pos, Angle* ang){
+    //update isNormalGame variable (to false if foul is different from KICKOFF, STOP, GAME_ON, PENALTY)
+    isNormalGame = false;
+    //update lastFoul variable (last foul different from GAME_ON, STOP)
+    lastFoul = VSSRef::GOAL_KICK;
+    //set behaviour doNothing so our player doesn't move from position emmited
+    setBehaviour(BHV_DONOTHING);
+
+    //put our goalkeeper in the middle of the goal
+    //if our side is right
+    if(loc()->ourSide().isRight()){
+        *pos = Position(true, loc()->fieldMaxX(), 0, 0);
+        *ang = Angle(true, Angle::pi);
+    }
+    //if our side is left
+    else{
+        *pos = Position(true, loc()->fieldMinX(), 0, 0);
+        *ang = Angle(true, 0);
+    }
+}
+
+void Role_Goalkeeper::kickOff(Position* pos, Angle* ang){
+    //update isNormalGame variable (to false if foul is different from KICKOFF, STOP, GAME_ON, PENALTY)
+    isNormalGame = true;
+    //update lastFoul variable (last foul different from GAME_ON, STOP)
+    lastFoul = VSSRef::KICKOFF;
+    //set behaviour doNothing so our player doesn't move from position emmited
+    setBehaviour(BHV_DONOTHING);
+
+    //put our goalkeeper in the middle of the goal
+    //if our side is right
+    if(loc()->ourSide().isRight()){
+        *pos = Position(true, loc()->fieldMaxX(), 0, 0);
+        *ang = Angle(true, float(M_PI_2));
+    }
+    //if our side is left
+    else{
+        *pos = Position(true, loc()->fieldMinX(), 0, 0);
+        *ang = Angle(true, float(M_PI_2));
+    }
+}
+
+void Role_Goalkeeper::freeBall(Position *pos, Angle *ang, VSSRef::Quadrant quadrant){
+    float gkYabs = 0.04f;
+
+    //update isNormalGame variable (to false if foul is different from KICKOFF, STOP, GAME_ON, PENALTY)
+    isNormalGame = true;
+    //update lastFoul variable (last foul different from GAME_ON, STOP)
+    lastFoul = VSSRef::FREE_BALL;
+    //set behaviour doNothing so our player doesn't move from position emmited
+    setBehaviour(BHV_DONOTHING);
+
+    //put our goalkeeper in the middle of the goal
+    //if our side is right
+    if(loc()->ourSide().isRight()){
+        if(quadrant == VSSRef::QUADRANT_1){
+            *pos = Position(true, loc()->fieldMaxX(), -1*gkYabs, 0);
+            *ang = Angle(true, float(M_PI_2));
+        }else if(quadrant == VSSRef::QUADRANT_4){
+            *pos = Position(true, loc()->fieldMaxX(), gkYabs, 0);
+            *ang = Angle(true, float(M_PI_2));
+        }else{
+            *pos = Position(true, loc()->fieldMaxX(), 0, 0);
+            *ang = Angle(true, float(M_PI_2));
+        }
+    }
+    //if our side is left
+    else{
+        if(quadrant == VSSRef::QUADRANT_2){
+            *pos = Position(true, loc()->fieldMinX(), -1*gkYabs, 0);
+            *ang = Angle(true, float(M_PI_2));
+        }else if(quadrant == VSSRef::QUADRANT_3){
+            *pos = Position(true, loc()->fieldMinX(), gkYabs, 0);
+            *ang = Angle(true, float(M_PI_2));
+        }else{
+            *pos = Position(true, loc()->fieldMinX(), 0, 0);
+            *ang = Angle(true, float(M_PI_2));
+        }
+    }
+}
+
+bool Role_Goalkeeper::ourTeamShouldTake(VSSRef::Color teamColor){
+    if(player()->team()->teamColor() == Colors::Color::BLUE){
+        if(teamColor == VSSRef::Color::BLUE) return true;
+        else return false;
+    }else if(player()->team()->teamColor() == Colors::Color::YELLOW){
+        if(teamColor == VSSRef::Color::YELLOW) return true;
+        else return false;
+    }else return false;
+}
+
+void Role_Goalkeeper::gameOn(){
+    if(!isNormalGame){
+        canGoBackToNormalGame = false;
+        if(weTake){
+            setBehaviour(BHV_TAKEFOUL);
+        }else{
+            setBehaviour(BHV_DONOTHING);
+        }
+    }else{
+        canGoBackToNormalGame = true;
+    }
 }
 
 void Role_Goalkeeper::receiveFoul(VSSRef::Foul foul, VSSRef::Quadrant quadrant, VSSRef::Color teamColor){
-    if(isInitialized() && player() != NULL)
-        emit emitPosition(player()->playerId(), Position(true, 0.0, 0.0, 0.0), Angle(true, 0.0));
+    if(isInitialized() && player() != NULL){
+        weTake = ourTeamShouldTake(teamColor);
+        Position pos;
+        Angle ang;
+        //PENALTY
+        if(foul == VSSRef::PENALTY_KICK){
+            penaltyKick(&pos, &ang);
+            emit emitPosition(player()->playerId(), pos, ang);
+        }
+        //GOAL KICK
+        else if(foul == VSSRef::GOAL_KICK){
+            goalKick(&pos, &ang);
+            emit emitPosition(player()->playerId(), pos, ang);
+        }
+        //STOP
+        else if(foul == VSSRef::STOP){
+            //set behaviour doNothing
+            setBehaviour(BHV_DONOTHING);
+        }
+        //GAME ON
+        else if(foul == VSSRef::GAME_ON){
+            gameOn();
+        }
+        //FREE BALL
+        else if(foul == VSSRef::FREE_BALL){
+            freeBall(&pos, &ang, quadrant);
+            emit emitPosition(player()->playerId(), pos, ang);
+        }
+        //KICK OFF
+        else if(foul == VSSRef::KICKOFF){
+            kickOff(&pos, &ang);
+            emit emitPosition(player()->playerId(), pos, ang);
+        }
+    }
 }
--- vss/src/entity/player/role/basics/role_goalkeeper.h
+++ vss/src/entity/player/role/basics/role_goalkeeper.h
@@ -29,11 +29,28 @@ class Role_Goalkeeper : public Role {
 private:
     // Behaviours
     Behaviour_Goalkeeper *_bh_gk;
+    Behaviour_DoNothing *_bh_dn;
+    Behaviour_TakeFoul *_bh_tf;
 
     // Behaviours Enum
     enum{
-        BHV_GK
+        BHV_GK,
+        BHV_DONOTHING,
+        BHV_TAKEFOUL
     };
+
+    //variables and functions
+    bool ourTeamShouldTake(VSSRef::Color teamColor);
+    void penaltyKick(Position* pos, Angle* ang);
+    void goalKick(Position* pos, Angle* ang);
+    void gameOn();
+    void freeBall(Position* pos, Angle* ang, VSSRef::Quadrant quadrant);
+    void kickOff(Position* pos, Angle* ang);
+
+    int lastFoul; //it can be FREE_KICK = 0, PENALTY_KICK = 1, GOAL_KICK = 2, FREE_BALL = 3, KICKOFF = 4
+    bool isNormalGame, canGoBackToNormalGame;
+    bool weTake;
+    long int counter;
 
     // Inherited functions
     void configure();
